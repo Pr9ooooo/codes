1de093b0349cce285f39273f91c4743f56daa8c4
Pr9ooooo <aaditsatapathy@gmail.com>
2024-12-17 18:47:11 +0530

    list2

---
+++ list_better.py
@@ -0,0 +1,14 @@
+list=[4,5,6,7,10,40]
+print('orignal list',list)
+count=0
+for i in list:
+    count=+1
+avg=count/len(list)
+print('avg',avg)
+print('sum',count)
+list.sort()
+list.reverse()
+print('largest element',list[-1])
+print('smallest element',list[0])
+
+
